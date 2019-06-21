dcb1a3821f0a36baa86400d5bfde58798334fccb
Pankaj Bat <pankaj.bat@lenskart.in>
2019-06-21 14:28:40 +0530

    added movie details call

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ const api_value = 'fc91acebf00c2840af37e04cecc67eb9';
 export const GET_LATEST_MOVIES_SUCCESS = 'GET_LATEST_MOVIES_SUCCESS';
 export const GET_TRENDING_MOVIES_SUCCESS = 'GET_LATEST_MOVIES_SUCCESS';
 export const GET_MOST_WATCHED_MOVIES_SUCCESS = 'GET_LATEST_MOVIES_SUCCESS';
+export const GET_MOVIE_DETAIL_SUCCESS = 'GET_MOVIE_DETAIL_SUCCESS';
 
 export function getLatestMoviesAsync() {
 	const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_value}&language=en-US&page=1`
@@ -29,6 +30,16 @@ export function getMostWatchedMoviesAsync() {
 		fetch(url)
 			.then((response) => response.json())
 			.then((data) => dispatch({ type: GET_MOST_WATCHED_MOVIES_SUCCESS, payload: data.results }))
+			.catch((e) => console.log("Error: " + e));
+	};
+}
+
+export function getMovieDetailsAsync(id) {
+	const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_value}&language=en-US&append_to_response=credits`
+	return dispatch => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => dispatch({ type: GET_MOVIE_DETAIL_SUCCESS, payload: data.results }))
 			.catch((e) => console.log("Error: " + e));
 	};
 }
