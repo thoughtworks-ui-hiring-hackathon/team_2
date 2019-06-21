9bee26ad0aef4d997d2a7aa5512c1b0e7b91a5bc
Pankaj Bat <pankaj.bat@lenskart.in>
2019-06-21 12:25:34 +0530

    added config & scss to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../scss/_header.scss';
 
 const Header = () => {
-    return (<div>
-        <Link to="/">Movie</Link>
-    </div>);
+    // const isExplore = window.location.href.indexOf('explore') > 0;
+    const isExplore = false;
+    return (
+        <div className="header">
+            <Link to="/" className="nav-link logo" >Movie App</Link>
+            <Link to="/" className={`nav-link ${isExplore ? '' : 'active'}`}>Home</Link>
+            <Link to="/explore" className={`nav-link ${isExplore ? 'active' : ''}`}>Explore</Link>
+        </div>
+    );
 }
 
 export default Header;
