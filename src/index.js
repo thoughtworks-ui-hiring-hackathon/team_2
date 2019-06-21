99dfd65d4e33d7fdcba8b0fdb41a4c3c5c45e8d4
Pankaj Bat <pankaj.bat@lenskart.in>
2019-06-21 11:52:05 +0530

    router setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Provider } from 'react-redux';
 
 import configureStore from './store';
+import 'font-awesome/css/font-awesome.min.css';
 import './scss/_index.css';
+
 import App from './app';
 
 ReactDOM.render(
 	<Provider store={configureStore()}>
-		<Router>
-			<Route path="/" component={App}/>
-		</Router>
+		<App />
 	</Provider>,
 	document.getElementById('root')
 );
